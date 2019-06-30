380629e9611ad99221a6825af43bcd2fb6aefb99
xueery <2042064451@qq.com>
2019-06-30 14:46:33 +0800

    加了静态变量

--- src/Interview.java
+++ src/Interview.java
@@ -1,23 +1,43 @@
 class Base{
+    static int Field=FieldStatic();
+    static int FieldStatic(){
+        System.out.println("静态变量初始化1");
+        return 0;
+    }
     static {
-        System.out.println("静态初始化");
+        System.out.println("静态代码块初始化1");
+    }
+    int field=field1();
+    int field1(){
+        System.out.println("变量初始化1");
+        return 0;
     }
     {
-        System.out.println("代码块构造");
+        System.out.println("代码块构造1");
     }
     Base(){
-        System.out.println("初始化");
+        System.out.println("构造方法初始化1");
     }
 }
 class Drived extends Base{
+    static int Field=FieldStatic();
+    static int FieldStatic(){
+        System.out.println("静态变量初始化2");
+        return 0;
+    }
     static{
-        System.out.println("静态初始化");
+        System.out.println("静态代码块初始化2");
+    }
+    int field=field1();
+    int field1(){
+        System.out.println("变量初始化2");
+        return 0;
     }
     {
-        System.out.println("代码块构造");
+        System.out.println("代码块构造2");
     }
     Drived() {
-        System.out.println("构造方法初始化");
+        System.out.println("构造方法初始化2");
     }
 }
 public class Interview {
