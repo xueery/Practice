ad1bc123f45857894e32c4e8541531028fe2b67b
xueery <2042064451@qq.com>
2019-09-12 12:37:54 +0800

    Node类

---
+++ src/Practice0824/Node.java
@@ -0,0 +1,10 @@
+package Practice0824;
+
+public class Node {
+    public int value;
+    public Node left=null;
+    public Node right=null;
+    public Node(int value){
+        this.value=value;
+    }
+}
