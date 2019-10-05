567058f175097c36c35d2ba5afaa4d86b738218c
xueery <2042064451@qq.com>
2019-10-05 11:02:40 +0800

    Node类

---
+++ src/Practice0904/Node.java
@@ -0,0 +1,14 @@
+package Practice0904;
+
+/**
+ * @author:wangxue
+ * @date:2019/9/5 20:06
+ */
+public class Node {
+    int val;
+    Node next=null;
+    Node random=null;
+    Node(int val) {
+        this.val = val;
+    }
+}
