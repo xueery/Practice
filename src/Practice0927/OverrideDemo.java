88c79016c2adb7fffd11fb0ed0b1c406b0ebdba0
xueery <2042064451@qq.com>
2019-10-28 07:48:42 +0800

    异常

---
+++ src/Practice0927/OverrideDemo.java
@@ -0,0 +1,21 @@
+package Practice0927;
+
+/**
+ * @author:wangxue
+ * @date:2019/9/27 18:55
+ */
+//关于方法的覆写，四个要求：1）方法名称相同 2）形参列表相同
+// 3）返回值相同或是它的子类型 4）访问限定符，不能比父类的严格 5)异常类型相同 OR 子类型
+public class OverrideDemo {
+    Animal get(){
+        return null;
+    };
+}
+class Animal{}
+class Bird extends Animal{}
+class OverrideExtend extends OverrideDemo{
+    @Override
+    Bird get() {
+        return null;
+    }
+}
