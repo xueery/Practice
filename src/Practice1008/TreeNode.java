87a1e8518dfa2ce462cdeee2ce97bd9251c9bed3
xueery <2042064451@qq.com>
2019-11-11 13:24:34 +0800

    TreeNode类

---
+++ src/Practice1008/TreeNode.java
@@ -0,0 +1,14 @@
+package Practice1008;
+
+/**
+ * @author:wangxue
+ * @date:2019/10/9 16:38
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val=val;
+    }
+}
