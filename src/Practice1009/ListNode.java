4caa501cb27510f991e3f0afcae269312c959b23
xueery <2042064451@qq.com>
2019-11-14 22:33:19 +0800

    ListNode类

---
+++ src/Practice1009/ListNode.java
@@ -0,0 +1,13 @@
+package Practice1009;
+
+/**
+ * @author:wangxue
+ * @date:2019/10/11 21:04
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+    }
+}
