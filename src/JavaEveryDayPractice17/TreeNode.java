90fa2b8959bb2b6a15def84d477919c30cb8ba3e
xueery <2042064451@qq.com>
2020-02-02 19:32:51 +0800

    牛客网题

---
+++ src/JavaEveryDayPractice17/TreeNode.java
@@ -0,0 +1,16 @@
+package JavaEveryDayPractice17;
+
+/**
+ * @author:wangxue
+ * @date:2019/11/24 9:13
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
