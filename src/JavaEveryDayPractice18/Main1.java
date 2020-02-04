6caa9bb9f97e3f568250162b4de21ad012bf5127
xueery <2042064451@qq.com>
2020-02-04 14:46:59 +0800

    牛客网题

---
+++ src/JavaEveryDayPractice18/Main1.java
@@ -0,0 +1,14 @@
+package JavaEveryDayPractice18;
+
+/**
+ * @author:wangxue
+ * @date:2019/11/25 19:25
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        int millionaire=10*30;
+        int poor=(1*(1-(int)Math.pow(2,30)))/(1-2);
+        System.out.print(millionaire+"");
+        System.out.print(poor);
+    }
+}
