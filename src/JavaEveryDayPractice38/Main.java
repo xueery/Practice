935f940c66a8a148b9ae884f725df14c820e1a3e
xueery <2042064451@qq.com>
2020-03-14 12:55:21 +0800

    牛客网题

---
+++ src/JavaEveryDayPractice38/Main.java
@@ -0,0 +1,43 @@
+package JavaEveryDayPractice38;
+
+import java.util.*;
+
+/**
+ * @author:wangxue
+ * @date:2020/3/8 19:51
+ */
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            int n = scan.nextInt();
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i=0; i<n; i++){
+                if(scan.nextInt() == 1){
+                    list.add(scan.nextInt());
+                }else{
+                    list.remove((Object)scan.nextInt());
+                }
+                canConstruct(list);
+                scan.nextLine();
+            }
+        }
+    }
+
+    public static void canConstruct(ArrayList<Integer> list){
+        if(list.size()<3){
+            System.out.println("No");
+            return;
+        }
+        Collections.sort(list);
+        int len=list.size(), sum=0;
+        for(int i=0; i<len-1; i++){
+            sum+=list.get(i);
+        }
+        if(sum<=list.get(len-1)){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+    }
+}
