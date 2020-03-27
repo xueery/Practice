eb9bf05829d45859ee6a9d8283fd8bdfe43d356b
xueery <2042064451@qq.com>
2020-03-27 12:17:04 +0800

    牛客网题

---
+++ src/JavaEveryDayPractice44/Main1.java
@@ -0,0 +1,26 @@
+package JavaEveryDayPractice44;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author:wangxue
+ * @date:2020/3/16 13:04
+ */
+public class Main1 {
+    public static List<List<Integer>> calSum(int n,int m){
+        for(int i=1;i<=n;i++){
+
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()){
+            int n=scanner.nextInt();
+            int m=scanner.nextInt();
+            List<List<Integer>> list=new ArrayList<>();
+        }
+    }
+}
