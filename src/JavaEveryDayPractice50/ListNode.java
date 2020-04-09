a61dfaae5d05474382547662fcaf66a3726d042f
xueery <2042064451@qq.com>
2020-04-09 08:53:35 +0800

    牛客网题

---
+++ src/JavaEveryDayPractice50/ListNode.java
@@ -0,0 +1,13 @@
+package JavaEveryDayPractice50;
+
+/**
+ * @author:wangxue
+ * @date:2020/3/25 16:21
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val=val;
+    }
+}
