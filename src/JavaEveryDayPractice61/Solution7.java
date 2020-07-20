435dabc4a477267fbbc9631b33c4efadf8503e4c
xueery <2042064451@qq.com>
2020-07-20 21:54:11 +0800

    牛客网题

---
+++ src/JavaEveryDayPractice61/Solution7.java
@@ -0,0 +1,21 @@
+package JavaEveryDayPractice61;
+
+import java.util.Scanner;
+
+/**
+ * @author:wangxue
+ * @date:2020/7/15 9:11
+ */
+public class Solution7 {
+    public static String calMultify(String s1,String s2){
+        return null;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNext()){
+            String s1=scanner.nextLine();
+            String s2=scanner.nextLine();
+            System.out.println(calMultify(s1,s2));
+        }
+    }
+}
