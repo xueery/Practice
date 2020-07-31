8578bffdd47b4bd5e0b0d65956a8c12ab3d7dbc7
xueery <2042064451@qq.com>
2020-07-31 22:37:54 +0800

    牛客网题

--- src/JavaEveryDayPractice61/Solution.java
+++ src/JavaEveryDayPractice61/Solution.java
@@ -24,6 +24,7 @@ public class Solution {
         }
         return ret;
     }
+
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
