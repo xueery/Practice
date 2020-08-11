3734ec9a27ea285a28549a3c93cfcb33851b2f41
xueery <2042064451@qq.com>
2020-08-11 08:26:37 +0800

    牛客网题

--- src/Practice0829/BubbleSort.java
+++ src/Practice0829/BubbleSort.java
@@ -24,6 +24,24 @@ public class BubbleSort {
         }
     }
 
+    public static void bubbleSortFirst(int[] array){
+        for(int i=0;i<array.length-1;i++){
+            boolean sort=true;
+            for(int j=0;j<array.length-1-i;i++){
+                if(array[j]>array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                    sort=false;
+                }
+            }
+            if(sort){
+                return;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         int[] array={3,4,8,7,0,1,7};
         bubbleSort(array);
