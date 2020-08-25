a27cea1446e7f76b81773b7c79a868ecccc83b69
xueery <2042064451@qq.com>
2020-08-25 22:46:49 +0800

    牛客网题

--- src/JavaEveryDayPractice61/Solution8.java
+++ src/JavaEveryDayPractice61/Solution8.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
  */
 // 两个负数相乘
 public class Solution8 {
-    
     public static int[] calComplexNumber(int[] arr1,int[] arr2){
         int n1=arr1[0]*arr2[0];
         int n2=arr1[0]*arr2[1];
