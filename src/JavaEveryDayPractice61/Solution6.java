ffa2321f681cbbe2c9c6ad8e47ee32c21b4f9a8f
xueery <2042064451@qq.com>
2020-09-02 12:52:11 +0800

    牛客网题

--- src/JavaEveryDayPractice61/Solution6.java
+++ src/JavaEveryDayPractice61/Solution6.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
  * @author:wangxue
  * @date:2020/7/15 8:52
  */
+
 // 输入一个"YYYY-MM-dd"格式的日期字符串，输出该天是当年的第几天（1 月 1 日是每年的第 1 天）
 public class Solution6 {
-    
+
     public static int calDays(int[] array){
         int year=array[0];
         boolean isLeap=false;
