65cb3389ce8f453d1b03699633bda4e46cf1c61c
xueery <2042064451@qq.com>
2021-03-06 22:21:00 +0800

    TreeNode

---
+++ src/JianzhiOffer/TreeNode.java
@@ -0,0 +1,15 @@
+package JianzhiOffer;
+
+/**
+ * @author:wangxue
+ * @date:2021/2/21 9:04
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
