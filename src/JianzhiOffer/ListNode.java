d5574f74e36852d3b47e881d11f9545457c9ad9b
xueery <2042064451@qq.com>
2021-03-07 19:36:48 +0800

    ListNode类

---
+++ src/JianzhiOffer/ListNode.java
@@ -0,0 +1,13 @@
+package JianzhiOffer;
+
+/**
+ * @author:wangxue
+ * @date:2021/2/27 10:23
+ */
+class ListNode {
+    int val;
+    ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
